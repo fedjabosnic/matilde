8aecda9496fbad22b52c4c963af92253bd4747a7
Fedja Bosnic <fedjabosnic@gmail.com>
2017-12-27 22:03:02 +0000

    Added skeleton project
    - Created application and test projects
    - Added fody costura to output single executable

---
+++ Matilde.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Matilde.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vagrant Inc.")]
+[assembly: AssemblyProduct("Matilde.Test")]
+[assembly: AssemblyCopyright("Copyright © Fedja Bosnic 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6f1421d4-275d-4e50-ba58-68254aedc37b")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ Matilde/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Matilde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Contains("--version") || args.Contains("-v"))
+            {
+                var version = Assembly.GetEntryAssembly().GetName().Version;
+
+                Console.WriteLine($"{version.Major}.{version.Minor}.{version.Revision}");
+                return;
+            }
+
+            Console.WriteLine("Matilde - your very own web api telekinesis superpower");
+        }
+    }
+}
